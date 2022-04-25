33e926090eae7e30c46df003bfd969bcb0e896c1
Hristijan Petrovski <kiko-bt@hotmail.com>
2022-04-25 23:14:15 +0200

    Initial commit for Advert.API project

---
+++ AdvertAPI/Services/IAdvertStorageService.cs
@@ -0,0 +1,10 @@
+﻿using AdvertApi.Models;
+
+namespace AdvertAPI.Services
+{
+    public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
---
+++ AdvertAPI/Services/Profiles/AdvertProfile.cs
@@ -0,0 +1,13 @@
+﻿using AdvertApi.Models;
+using AutoMapper;
+
+namespace AdvertAPI.Services.Profiles
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, AdvertDBModel>();
+        }
+    }
+}
